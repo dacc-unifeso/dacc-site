9a726f465ed9c6b18176a3338761ada94b6892de
Yuri Domingues <yuridomingues.contato@gmail.com>
2026-02-23 14:27:17 -0300

    feat: add new products in lojinha

--- src/config/products.js
+++ src/config/products.js
@@ -3,8 +3,9 @@
  * @constant {Array<{ name: string, price: string, tag: string, category: string }>}
  */
 export const PRODUCTS = [
-  { name: 'Camiseta DACC', price: 'XX,XX', tag: 'Algodão', category: 'Vestuário' },
+  { name: 'Camiseta DACC', price: '55,00', tag: 'Algodão', category: 'Vestuário' },
   { name: 'Camiseta Alternativa DACC', price: 'XX,XX', tag: 'Algodão', category: 'Vestuário' },
   { name: 'Sticker Pack Gato', price: 'XX,XX', tag: 'Vinil', category: 'Acessórios' },
-  { name: 'Caneca DACC', price: 'XX,XX', tag: 'Térmica', category: 'Acessórios' },
+  { name: 'Caneca Térmica DACC', price: 'XX,XX', tag: 'Térmica', category: 'Acessórios' },
+  { name: 'Caneca Cerâmica', price: 'XX,XX', tag: 'Cerâmica', category: 'Acessórios' },
 ]
