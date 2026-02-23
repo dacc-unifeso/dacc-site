b7fbfbb21ca3078106ba1d396fdd71fc035ef09a
Yuri Domingues <yuridomingues.contato@gmail.com>
2026-02-23 14:36:57 -0300

    fix: change product price

--- src/config/products.js
+++ src/config/products.js
@@ -3,7 +3,7 @@
  * @constant {Array<{ name: string, price: string, tag: string, category: string }>}
  */
 export const PRODUCTS = [
-  { name: 'Camiseta DACC', price: '55,00', tag: 'Algodão', category: 'Vestuário' },
+  { name: 'Camiseta DACC', price: '55,90', tag: 'Algodão', category: 'Vestuário' },
   { name: 'Camiseta Alternativa DACC', price: 'XX,XX', tag: 'Algodão', category: 'Vestuário' },
   { name: 'Sticker Pack Gato', price: 'XX,XX', tag: 'Vinil', category: 'Acessórios' },
   { name: 'Caneca Térmica DACC', price: 'XX,XX', tag: 'Térmica', category: 'Acessórios' },
